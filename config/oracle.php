c9e3835b5b14f236c7740501f363aac2a01de0c3
Javier Alarcon <javier.alarcon25@gmail.com>
2016-11-16 13:30:55 -0400

    Agregar conexion a otra bd oracle (Conexion a vistas de software nomina)

--- config/oracle.php
+++ config/oracle.php
@@ -14,4 +14,18 @@ return [
         'prefix_schema' => env('DB_SCHEMA_PREFIX', ''),
         //'schema'        => env('DB_SCHEMA',''),
     ],
+
+    'oracle_ex' => [
+        'driver'        => 'oracle',
+        'tns'           => env('DB_TNS_EX', ''),
+        'host'          => env('DB_HOST_EX', ''),
+        'port'          => env('DB_PORT_EX', '1521'),
+        'database'      => env('DB_DATABASE_EX', ''),
+        'username'      => env('DB_USERNAME_EX', ''),
+        'password'      => env('DB_PASSWORD_EX', ''),
+        'charset'       => env('DB_CHARSET_EX', 'AL32UTF8'),
+        'prefix'        => env('DB_PREFIX_EX', ''),
+        'prefix_schema' => env('DB_SCHEMA_PREFIX_EX', ''),
+        //'schema'        => env('DB_SCHEMA_EX',''),
+    ],
 ];
